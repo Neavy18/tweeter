e225e5713d2633921cfae3657a44647f59cf41ac
Neavy18 <genevieve.gt18@gmail.com>
2021-12-15 21:30:13 -0500

    Implement dynamic tweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,10 +5,42 @@
  */
 
 
-$(document).ready(function(){
+$(document).ready(function() {
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    tweets.forEach((tw) => {
+      let tweet = createTweetElement(tw);
+      $('#tweets-container').prepend(tweet);
+    });
+
+  };
 
   const createTweetElement = function(tweet) {
-    
     let $tweet = $(`
     <article>
       <header>
@@ -20,17 +52,19 @@ $(document).ready(function(){
       </header>
       <p>${tweet.content.text}</p>
       <footer>
-        <span>${tweet.created_at}</span>
+        <span>${timeago.format(tweet.created_at)}</span>
         <div class = "icons">
           <i class="fa-solid fa-flag flag"></i>
           <i class="fa-solid fa-share share"></i>
           <i class="fa-solid fa-heart heart"></i> 
         </div>
       </footer>
-  </article>`)
-    return $tweet
-  }
-
-$('.tweet-container').append($tweet); 
-})
+  </article>`);
+  
+    return $tweet;
+  
+  };
+  
+  renderTweets(data);
+});
 
