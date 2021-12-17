feae108352b2a824f8a6674192f45faef4103831
Neavy18 <genevieve.gt18@gmail.com>
2021-12-16 19:42:47 -0500

    GET tweet with ajax request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,7 @@
 
 
 $(document).ready(function() {
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
+  
   const renderTweets = function(tweets) {
     tweets.forEach((tw) => {
       let tweet = createTweetElement(tw);
@@ -64,25 +39,24 @@ $(document).ready(function() {
     return $tweet;
   
   };
-  
-  renderTweets(data);
 
-  //.submit(); x
-  //.preventDefault(); x
-  //.ajax({get tweet})
-  //.serializeData
-  //.catch(error)
-
-  $(".post-tweet").submit(function(event){
-    event.preventDefault()
+  $(".post-tweet").submit(function(event) {
+    event.preventDefault();
     $.ajax("/tweets", {
       method: "POST",
       data: $(this).serialize()
     })
-    .then((data) => {
-      console.log(data)
-      
+      .then((data) => {});
+  });
+
+  const loadTweets = function() {
+    $.ajax("/tweets", {
+      method: "GET"
     })
-  })
+      .then((data) => {
+        renderTweets(data);
+      });
+  };
+  loadTweets();
 });
 
